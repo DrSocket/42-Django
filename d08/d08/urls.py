2aacba745623d4622cc4747d98892ce615d15122
Lucien-alexandre SANCHEZ-ZUBER <lucisanc@e2r4p4.clusters.42paris.fr>
2022-10-23 19:26:49 +0200

    final

--- d08/d08/urls.py
+++ d08/d08/urls.py
@@ -1,22 +1,16 @@
-"""d08 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from rest_framework.routers import DefaultRouter
+from core.api import MessageModelViewSet, UserModelViewSet
+
+router = DefaultRouter()
+router.register(r'message', MessageModelViewSet, basename='message-api')
+router.register(r'user', UserModelViewSet, basename='user-api')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(('account.urls', "account"), namespace='account')),
+    path(r'api/v1/', include(router.urls)),
+
+    path('', login_required(
+        TemplateView.as_view(template_name='d08/chat.html')), name='home'),
 ]
